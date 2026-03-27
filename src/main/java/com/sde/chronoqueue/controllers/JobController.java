e515c3f106521016643f1e1adeb6b2b193afbe22
Anvesh Yadav <anveshyadavmokam@gmail.com>
2026-03-27 15:04:49 +0530

    cors-ip

--- src/main/java/com/sde/chronoqueue/controllers/JobController.java
+++ src/main/java/com/sde/chronoqueue/controllers/JobController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://16.112.57.1")
 @RestController
 @RequestMapping("/api/jobs")
 @RequiredArgsConstructor
